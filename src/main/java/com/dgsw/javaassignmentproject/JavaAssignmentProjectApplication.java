e9d0e444be1bba4a0cec1633086861d5dd218d91
EUNGWANCHOI <silvergwan@gmail.com>
2025-11-20 12:16:57 +0900

    init: SpringBoot project setting

---
+++ src/main/java/com/dgsw/javaassignmentproject/JavaAssignmentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.javaassignmentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssignmentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAssignmentProjectApplication.class, args);
+    }
+
+}
